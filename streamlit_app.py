a61675222f6c9f8afb333ebfa62eebf0edbe8ee5
soma3004 <so.uma2008m@gmail.com>
2024-07-29 14:31:49 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 problems=[
-    {"line1":"下記は何を求める公式か？","line2":"r'\bar{v}=\frac{\Delta x}{\Delta t}'","answer":"変位"},
+    {"question":"私は［空白］に住んでいます。","answer":"東京"},
     {"question":"太陽は［空白］から昇ります。","answer":"東"},
     {"question":"富士山は［空白］にあります。","answer":"日本"},
 ] 
