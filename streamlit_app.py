1955979dc4ad680e36d37d7a55239685c1edb390
soma3004 <so.uma2008m@gmail.com>
2024-08-05 14:06:17 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,23 +119,37 @@ st.title("単位の変換")
 selected_item=st.selectbox("",[
                            "長さ","時間","質量","面積","体積","時速","分速","秒速","密度","圧力","仕事","仕事率","電流","SI接頭語"])
 
+
 def generate_problem():
     a = random.randint(1,9)
     b = random.randint(1,9)
     return a,b
 
-a,b = generate_problem()
-correct_answer = a*b
+if 'current_problem' not in st.sessuin_state:
+    st.session_state.current_problem = generate_problem()
+    st.session_state.user_answer = None
+    st.session_state.corrent_answer = st.session_state.current_problem[0]
+    st.session_state.result_message = None
 
+
+a,b = st.session_state.current_problem
 st.write(f"問題:{a} × {b} = ?")
 user_answer = st.number_input("答えを入力してください",min_value=0,step=1)
 if st.button("回答を確認"):
-    if user_answer == correct_answer:
+    st.session_state.user_answer = user_answer
+    if user_answer == st.session_state.correct_answer:
         st.success("正解です！")
     else:
-        st.error(f"不正解です。正しい答えは{correct_answer}")
+        st.error(f"不正解です。正しい答えは{st.session_state.correct_answer}です。")
+
+    st.session_state.current_problem = generate_problem()
+    st.session_state.correct_answer = st.session_state.current_problem[0]*st.session_state.current_problem[1]
+
+if st.session_state.result_message:
+    st.write(st.session_state.result_message)
 
 if st.button("再挑戦"):
-    a,b = generate_problem()
-    correct_answer = a*b
-    st.write(f"新しい問題:{a} × {b} = ?")
+    st.session_state.current_problem = generate_problem()
+    st.session_state.current_answer = st.session_state.current_problem[0]*st.session_state.current_problem[1]
+    st.session_state.result_message = None
+    st.session_state.user_answer = None
