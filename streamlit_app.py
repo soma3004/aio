a159bfe5e7138637a32b0d243acf42332d21ab9f
soma3004 <so.uma2008m@gmail.com>
2024-08-05 16:01:42 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,13 @@ if selection == '変位':
     st.write("")
     st.latex(r'\bar{v}=\frac{\Delta x}{\Delta t}')
 elif selection == '速度':
-    st.subheader("速度")
+    col1, col2 = st.columns([3,1])
+    with col1:
+        st.subheader("速さ")
+    with col2:
+        st.write("単位時間あたりの移動距離")
     st.latex(r'\bar{v}=\frac{x_2-x_1}{t_2-t_1}=\frac{\Delta x}{\Delta t}')
     st.write("　　ｖ：速さ　　ｘ：移動距離　　ｔ：経過時間")
-    st.write("単位時間当たりの移動距離")
     st.text("単位：ｍ／ｓ（メートル毎秒）etc")
     
 elif selection == '等速直線運動':
