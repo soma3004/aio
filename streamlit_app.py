7eb1cb3d84ad165a6147c32022181832e995cea6
soma3004 <so.uma2008m@gmail.com>
2024-08-05 16:03:12 +0000

    q

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ if selection == '変位':
 elif selection == '速度':
     col1, col2 = st.columns([1,3])
     with col1:
-        st.subheader("速さ")
+        st.title("速さ")
     with col2:
         st.write("単位時間あたりの移動距離")
     st.latex(r'\bar{v}=\frac{x_2-x_1}{t_2-t_1}=\frac{\Delta x}{\Delta t}')
