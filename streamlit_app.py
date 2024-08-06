0b757a042a7ad39948711662263c8084b9d2e8fc
soma3004 <so.uma2008m@gmail.com>
2024-08-06 01:25:18 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("物理基礎")
 
 st.header("物理基礎の公式")
 
-options = ['平均の速度','速度','等速直線運動','合成速度','相対速度','加速度','等加速度直線運動','鉛直投げ下ろし','鉛直投げ上げ','自由落下','水平投射','斜方投射']
+options = ['平均の速度','速度','等速直線運動','合成速度','相対速度','加速度','等加速度直線運動','鉛直投げ下ろし','鉛直投げ上げ','自由落下','水平投射','斜方投射','重力','フックの法則','圧力','水圧','浮力']
 selection = st.selectbox("表示したい公式を選んでください:",options)
 if selection == '平均の速度':
     st.subheader("平均の速度")
@@ -86,6 +86,33 @@ elif selection == '斜方投射':
     st.latex(r'v_y^2-(v_osinθ)^2=-sgy')
     st.latex(r'y=tanθx-\frac{g}{2v_o^2cos^2θ}x^2')
     st.write("v：速度　　Vo：初速度　　Vx：速度vのx成分　　Vy：速度vのy成分　　x：水平方向の変位　　y：鉛直方向の変位　　t：経過時間　　g：重力加速度　　θ:水平方向となす角度")
-
-
+elif selection == '重力':
+    st.subheader("重力")
+    st.write("物体が地球からうける力。鉛直下向きにはたらく")
+    st.latex(r'w=mg')
+    st.write("w：重力の大きさ（重さ）　　m：物体の質量　　g：重力加速度")
+    st.text("単位：N（ニュートン）")
+elif selection == 'フックの法則':
+    st.subheader("フックの法則")
+    st.write("弾性力の大きさはバネの伸びに比例する。")
+    st.latex(r'F=kx')
+    st.write("F：弾性力　　k：バネ定数　　x：バネの伸び")
+elif selection == '圧力':
+    st.subheader("圧力")
+    st.write("単位面積あたりの力")
+    st.latex(r'P=\frac{F}{S}')
+    st.write("P：圧力　　F：力　　S：面積")
+    st.text("単位：Pa（パスカル）,hPa（ヘクトパスカル）etc")
+elif selection == '水圧':
+    st.subheader("水圧")
+    st.write("水による圧力")
+    st.latex(r'P=phg')
+    st.write("P：水圧　　p：水の密度　　h：水深　　g：重力加速度")
+    st.text("単位：Pa（パスカル）")
+elif selection == '浮力':
+    st.subheader("浮力")
+    st.write("流体から物体へ、重力と反対向きにはたらく力")
+    st.latex(r'F=pVg')
+    st.write("F：浮力の大きさ　　p：流体の密度　　V：物体が排除した流体の体積　　g：重力加速度")
+    st.text("単位：N（ニュートン）")    
 
