c04058a0c0ad967cea2542396b6fce308189f608
soma3004 <so.uma2008m@gmail.com>
2024-08-06 00:31:32 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,37 +26,56 @@ elif selection == '等速直線運動':
     st.text("単位：ｍ／ｓ（メートル毎秒）etc")
 elif selection == '合成速度':
     st.subheader("合成速度")
+    st.write("速さ１と速さ２の和の速さ")
     st.latex(r'\vec{v}=\vec{v_1}+\vec{v_2}')
+    st.write("ｖ：速さ　　ｖ１：速度１　　ｖ２：速度２")
+    st.text("単位：ｍ／ｓ（メートル毎秒")
 elif selection == '相対速度':
     st.subheader("相対速度")
+    st.wite("動く物体Ａから観察したほかの物体Ｂの速度のこと")
     st.latex(r'\vec{v_(ab)}=\vec{v_b}-\vec{v_a}')
+    st.write("Ｖab：Ａに対するＢの相対速度　　Ｖa：物体Ａ（観察者）の速度　　Ｖb：物体Ｂ（相手）の速度")
+    st.text("単位：ｍ／ｓ（メートル毎秒）")
 elif selection == '加速度':
     st.subheader("加速度")
-    st.latex(r'a=\frac{x_2-x_1}{t_2-t_1}')
+    st.write("単位時間あたりの速度の変化")
+    st.latex(r'a=\frac{Delta x}{Delta t}=\frac{x_2-x_1}{t_2-t_1}')
+    st.write("a：加速度　　Deltaｘ：速度の変化　　Deltaｔ：経過時間")
+    st.text("単位：ｍ／ｓ^2（メートル毎秒毎秒）  ")
 elif selection == '等加速度直線運動':
     st.subheader("等加速度直線運動")
+    st.write("一直線上を一定の加速度で進む運動")
     st.latex(r'v=v_o+at')
     st.latex(r'x=v_ot+\frac{1}{2}at^2')
     st.latex(r'v^2-v_o^2=2ax')
+    st.write("Ｖ：速度　　Ｖo：初速度　　a：加速度　　t：経過時間　　x：変位")
 elif selection == '自由落下':
     st.subheader("自由落下")
+    st.write("物体が重力だけを受け、初速度０で鉛直下向きに落下する運動")
     st.latex(r'v=gt')
     st.latex(r'y=\frac{1}{2}gt^2')
     st.latex(r'v^2=2gy')
+    st.write("V：速度　　g：重力加速度　　t：経過時間　　y：変位")
 elif selection == '鉛直投げ下ろし':
     st.subheader("鉛直投げ下ろし")
+    st.write("鉛直下向きに初速度Voで投げる運動")
     st.latex(r'v=v_o+gt')
     st.latex(r'y=v_ot+\frac{1}{2}gt^2')
     st.latex(r'v^2-v_o^2=2gy')
+    st.write("V：速度　　Vo:初速度　　g：重力加速度　　t：経過時間　　y：変位")
 elif selection == '鉛直投げ上げ':
     st.subheader("鉛直投げ上げ")
+    st.write("")
     st.latex(r'v=v_o-gt')
     st.latex(r'y=v_o-\frac{1}{2}gt^2')
-    st.latex(r'v^2-v_o^2=-agy')
+    st.latex(r'v^2-v_o^2=-2ag')
+    st.write("V：速さ　　Vo：初速度　　g：重力加速度　　t：経過時間　　y：変位")
 elif selection == '水平投射':
     st.write("水平投射")
+    st.write("物体をある高さから水平方向に投げ出す運動")
     st.latex(r'x=v_ot')
     st.latex(r'y=\frac{1}{2}gt^2')
+    st.write("x：水平方向の変位　　y：鉛直方向の変位　　Vo：初速度　　t：経過時間　　g：重力加速度")
 
 
 
